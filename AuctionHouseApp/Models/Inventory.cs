4e1e627dbf2952422c8acdb91b42973357058d48
rafalblaszczykdev <rafalblaszczykdev@gmail.com>
2020-05-25 15:56:50 +0200

    First commit ver2020

---
+++ AuctionHouseApp/Models/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionHouseApp.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+
+        [Range(1, 999, ErrorMessage = "Price should be greater than 1 and smaller then 1000")]
+        public int Count { get; set; }
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Game Item")]
+        public int GameItemId { get; set; }
+        [ForeignKey("GameItemId")]
+        public virtual GameItem GameItemVP { get; set; }
+
+
+        public string GameUserId { get; set; }
+
+        [NotMapped]
+        [ForeignKey("GameUserId")]
+        public virtual GameUser GameUserVP { get; set; }
+    }
+}
